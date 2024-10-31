3ce3aa66a1f341f84912c16169080b2ddd54f58e
Benedict Thompson <me@benjft.uk>
2024-10-31 15:07:07 +0000

    Refactor `GetTypesFromLoadedAssembly` for assembly filtering

--- Loxley.Core/Extensions/AssemblyExtensions.cs
+++ Loxley.Core/Extensions/AssemblyExtensions.cs
@@ -3,13 +3,24 @@
 namespace Loxley.Core.Extensions;
 
 public static class AssemblyExtensions {
-    public static IEnumerable<Type> GetTypesFromLoadedAssembly(this Assembly assembly) {
-        Assembly[] assemblies = [
-            assembly,
-            ..assembly.GetReferencedAssemblies()
-                      .Select(Assembly.Load)
-        ];
+    public static IEnumerable<Type> GetTypesFromLoadedAssembly(this Assembly assembly,
+                                                               string        assemblyFilter = nameof(Loxley)) {
+        return assembly.GetAllReferencedAssemblies(assemblyFilter).SelectMany(a => a.GetTypes());
+    }
 
-        return assemblies.SelectMany(a => a.GetTypes());
+    private static IEnumerable<Assembly> GetAllReferencedAssemblies(this Assembly assembly, string assemblyFilter) {
+        var assemblies = InnerGetAllReferencedAssemblies(assembly);
+        return assemblies.DistinctBy(asm => asm.FullName);
+
+        IEnumerable<Assembly> InnerGetAllReferencedAssemblies(Assembly parentAssembly) {
+            yield return parentAssembly;
+            foreach (var assemblyName in parentAssembly.GetReferencedAssemblies()
+                                                       .Where(a => a.Name?.Contains(assemblyFilter) ?? false)) {
+                var nextAssembly = Assembly.Load(assemblyName);
+                foreach (var childAssembly in InnerGetAllReferencedAssemblies(nextAssembly)) {
+                    yield return childAssembly;
+                }
+            }
+        }
     }
 }
